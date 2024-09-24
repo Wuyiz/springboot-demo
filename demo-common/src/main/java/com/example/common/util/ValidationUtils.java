fe1b85c5e6d15b4b4baeb458c73db13db6bc499d
wuyiz <wuyiz@foxmail.com>
2024-09-24 09:40:13 +0800

    feat: ValidationUtils新增功能

--- demo-common/src/main/java/com/example/common/util/ValidationUtils.java
+++ demo-common/src/main/java/com/example/common/util/ValidationUtils.java
@@ -19,14 +19,54 @@ public class ValidationUtils {
     }
 
     /**
-     * 校验对象，校验不通过抛出异常
+     * 校验bean对象
+     *
+     * @param <T>    Bean类型
+     * @param bean   bean
+     * @param groups 校验组
+     * @return {@link Set} 校验结果集
+     */
+    public static <T> Set<ConstraintViolation<T>> validate(T bean, Class<?>... groups) {
+        return validator.validate(bean, groups);
+    }
+
+    /**
+     * 校验bean的某一个属性
+     *
+     * @param <T>          Bean类型
+     * @param bean         bean
+     * @param propertyName 属性名称
+     * @param groups       验证分组
+     * @return {@link Set}
+     */
+    public static <T> Set<ConstraintViolation<T>> validateProperty(T bean, String propertyName, Class<?>... groups) {
+        return validator.validateProperty(bean, propertyName, groups);
+    }
+
+    /**
+     * 校验bean对象，校验不通过抛出异常{@link ConstraintViolationException}
      *
      * @param <T>    Bean类型
      * @param bean   被校验的Bean
      * @param groups 校验组
      */
-    public static <T> void validate(T bean, Class<?>... groups) {
-        Set<ConstraintViolation<T>> result = validator.validate(bean, groups);
+    public static <T> void validateAndThrowException(T bean, Class<?>... groups) {
+        Set<ConstraintViolation<T>> result = validate(bean, groups);
+        if (!result.isEmpty()) {
+            throw new ConstraintViolationException(result);
+        }
+    }
+
+    /**
+     * 校验bean对象的某一个属性，校验不通过抛出异常{@link ConstraintViolationException}
+     *
+     * @param <T>          Bean类型
+     * @param bean         被校验的Bean
+     * @param propertyName 属性名称
+     * @param groups       校验组
+     */
+    public static <T> void validatePropertyAndThrowException(T bean, String propertyName, Class<?>... groups) {
+        Set<ConstraintViolation<T>> result = validateProperty(bean, propertyName, groups);
         if (!result.isEmpty()) {
             throw new ConstraintViolationException(result);
         }
