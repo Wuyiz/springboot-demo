c4ecd149bfb417e19c8fac46bc6dfe523d365241
wuyiz <wuyiz@foxmail.com>
2024-11-27 14:26:01 +0800

    feat: GeoUtils距离单位精度调整为米

--- demo-common/src/main/java/com/example/common/util/GeoUtils.java
+++ demo-common/src/main/java/com/example/common/util/GeoUtils.java
@@ -8,9 +8,9 @@ package com.example.common.util;
  */
 public class GeoUtils {
     /**
-     * 地球半径的默认值（单位：公里）
+     * 地球半径的默认值（单位：米）
      */
-    private static final double DEFAULT_EARTH_RADIUS = 6371.0;
+    private static final double DEFAULT_EARTH_RADIUS = 6371000;
 
     /**
      * 根据两个点的经纬度计算它们之间的球面距离（大圆距离）
@@ -21,7 +21,7 @@ public class GeoUtils {
      * @param latA 第一个点的纬度
      * @param lngB 第二个点的经度
      * @param latB 第二个点的纬度
-     * @return 两个点之间的球面距离（单位：公里）
+     * @return 两个点之间的球面距离（单位：米）
      */
     public static double earthDistanceOfHaversine(double lngA, double latA, double lngB, double latB) {
         return earthDistanceOfHaversine(lngA, latA, lngB, latB, DEFAULT_EARTH_RADIUS);
@@ -35,10 +35,10 @@ public class GeoUtils {
      * @param latA        第一个点的纬度
      * @param lngB        第二个点的经度
      * @param latB        第二个点的纬度
-     * @param earthRadius 地球半径（单位：公里）
-     * @return 两个点之间的球面距离（单位：公里）
+     * @param earthRadius 地球半径（单位：米）
+     * @return 两个点之间的球面距离（单位：米）
      */
-    public static double earthDistanceOfHaversine(double lngA, double latA, double lngB, double latB, double earthRadius) {
+    public static long earthDistanceOfHaversine(double lngA, double latA, double lngB, double latB, double earthRadius) {
         double lngARad = Math.toRadians(lngA);
         double latARad = Math.toRadians(latA);
         double lngBRad = Math.toRadians(lngB);
@@ -50,6 +50,6 @@ public class GeoUtils {
                 Math.cos(latARad) * Math.cos(latBRad) * Math.pow(Math.sin(lngRadDiff / 2), 2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-        return earthRadius * c;
+        return (long) (earthRadius * c);
     }
 }
